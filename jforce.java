15552ce9dd22fd1af022352df28d226bced8a16a
EH30 <games.eh2000@gmail.com>
2019-12-18 16:04:10 +0530

    Add files via upload

--- jforce.java
+++ jforce.java
@@ -1,14 +1,16 @@
-import java.util.Scanner;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.io.IOException;
 import java.math.BigInteger;
 
 
 class jforce{
     
-    public static String md2(String str){
+    public static String os = System.getProperty("os.name").toLowerCase();
+
+    public static String crypto(String str, String hashing){
         try {
-            MessageDigest md = MessageDigest.getInstance("MD2");
+            MessageDigest md = MessageDigest.getInstance(hashing);
             byte[] digestmessage = md.digest(str.getBytes());
 
             BigInteger b = new BigInteger(1, digestmessage);
@@ -19,154 +21,43 @@ class jforce{
             }
             return hashtext.toString();
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+        return null;
         
-    }
-    
-    public static String md5(String str){
-        try {
-            MessageDigest md = MessageDigest.getInstance("md5");
-            byte[] digestmessage = md.digest(str.getBytes());
-
-            BigInteger b = new BigInteger(1, digestmessage);
-            String hashtext = b.toString(16);
-
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
-
-    public static String sha224(String str){
-        try {
-            MessageDigest md = MessageDigest.getInstance("sha-224");
-            byte[] messagedigest = md.digest(str.getBytes());
-
-            BigInteger b = new BigInteger(1, messagedigest);
-
-            String hashtext = b.toString(16);
-
-            while(hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-
-            return hashtext.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String sha256(String str){
-        try {
-            MessageDigest md = MessageDigest.getInstance("sha-256");
-            byte[] messagedigest = md.digest(str.getBytes());
-
-            BigInteger b = new BigInteger(1, messagedigest);
-            String hashtext = b.toString(16);
-
-            while (hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-            return hashtext.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String sha384(String str) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("sha-384");
-            byte[] messagedigest = md.digest(str.getBytes());
-
-            BigInteger b = new BigInteger(1, messagedigest);
-
-            String hashtext = b.toString(16);
-
-            while (hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-
-            return hashtext.toString();
-
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        
-    }
-    public static String sha1(String str){
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            byte[] digestmessage = md.digest(str.getBytes());
-
-            BigInteger e = new BigInteger(1, digestmessage);
-
-            String hashtext = e.toString(16);
-
-            while (hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-            
-            return hashtext.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String sha512(String str){
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] messagedigest = md.digest(str.getBytes());
-
-            BigInteger b = new BigInteger(1, messagedigest);
-
-            String hashtext = b.toString(16);
-
-            while (hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-            return hashtext.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static void combo(int max, char[] chr, String data, String hashed){
         if (data.length() == max){
             //System.out.println(crypto(data) + ": " + hashed + ":" + data);
-
-            if (hashed.trim().equals(md5(data)) | hashed.trim().equals(md5(data).toUpperCase())){
-                System.out.println("Type: md5 Cracked: " + data);
-                System.exit(1);
+            System.out.print(String.format("\r \u001B[36m Trying: %s  Lngth: %d \u001B[0m", data, max));
+            if (hashed.trim().equals(crypto(data, "MD5")) | hashed.trim().equals(crypto(data, "MD5").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: MD5 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(sha1(data)) | hashed.trim().equals(sha1(data).toUpperCase())){
-                System.out.println("Type: sha1 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "SHA1")) | hashed.trim().equals(crypto(data, "SHA1").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: SHA1 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(sha224(data)) | hashed.trim().equals(sha224(data).toUpperCase())){
-                System.out.println("Type: sha224 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "SHA-224")) | hashed.trim().equals(crypto(data, "SHA-224").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: SHA-224 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(sha256(data)) | hashed.trim().equals(sha256(data).toUpperCase())){
-                System.out.println("Type: sha256 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "SHA-256")) | hashed.trim().equals(crypto(data, "SHA-256").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: SHA-256 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(sha384(data)) | hashed.trim().equals(sha384(data).toUpperCase())){
-                System.out.println("Type: sha384 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "SHA-384")) | hashed.trim().equals(crypto(data, "SHA-384").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: SHA-384 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(sha512(data)) | hashed.trim().equals(sha512(data).toUpperCase())){
-                System.out.println("Type: sha512 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "SHA-512")) | hashed.trim().equals(crypto(data, "SHA-512").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: SHA-512 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
-            else if (hashed.trim().equals(md2(data)) | hashed.trim().equals(md2(data).toUpperCase())){
-                System.out.println("Type: md2 Cracked: " + data);
-                System.exit(1);
+            else if (hashed.trim().equals(crypto(data, "MD2")) | hashed.trim().equals(crypto(data, "MD2").toUpperCase())){
+                System.out.println(String.format("\u001B[36m Type: MD2 Cracked: %s \u001B[0m", data));
+                System.exit(0);
             }
 
         }else{
@@ -180,23 +71,42 @@ class jforce{
             }   
         }
     }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Hash: ");
-        String hashed = input.nextLine();
-
-        System.out.print("Length: ");
-        int len = input.nextInt();
-        input.close();
-
-        char[] wdlist = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z', 'y',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', 'Y', '/', '.',
-         ';', '"', ']', '[', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ':', '|', ',', '=', '-', '_', '!', '@', '#', '$', '%', '^', 
-         '&', '*', '(' ,')' ,'~', '`', 92, 39};         
-         //'\ ', "'",  '`'
-
-        combo(len, wdlist, "", hashed);
+    public static void clear_console(){
+        try {
+            if(oswindows()){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }else if(oslinux()){
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         
     }
+
+
+    public static void main(String[] args) {
+        clear_console();
+
+        try {
+            int len = Integer.parseInt(args[0]);
+            String hashed = args[1];
+
+            char[] wdlist = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z', 'y',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', 'Y', '/', '.',
+            ';', '"', ']', '[', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ':', '|', ',', '=', '-', '_', '!', '@', '#', '$', '%', '^', 
+            '&', '*', '(' ,')' ,'~', '`', 92, 39};
+            //'\ ', "'",  '`'
+            
+            combo(len, wdlist, "", hashed);
+        } catch (java.lang.StringIndexOutOfBoundsException e) {
+            System.out.println("Usage: java jforce [Length] [Hash]");
+        }    
+    }
+    public static boolean oswindows(){return os.contains("windows") | os.contains("win");}
+    public static boolean oslinux(){return os.contains("linux") | os.contains("unix");}
 }
